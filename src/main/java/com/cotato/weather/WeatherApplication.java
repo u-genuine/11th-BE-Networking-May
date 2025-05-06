08815b02c2e698c2aea0c481c7aa95fe160a9115
u-genuine <u_genuine@naver.com>
2025-05-06 11:36:01 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/cotato/weather/WeatherApplication.java
@@ -0,0 +1,15 @@
+package com.cotato.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class WeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+}
