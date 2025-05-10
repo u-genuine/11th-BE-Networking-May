40d5e1c2ffee9f174adaa7fd435a2c8c957afce7
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-05-10 16:56:13 +0900

    feat : RestTemplate Bean 등록

--- src/main/java/com/cotato/weather/WeatherApplication.java
+++ src/main/java/com/cotato/weather/WeatherApplication.java
@@ -2,7 +2,9 @@ package com.cotato.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class WeatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
