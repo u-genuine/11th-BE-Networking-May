f72d042750e1986b21c893fc74b6bd841987ecf7
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-05-10 16:55:21 +0900

    feat : 외부 API 설정

---
+++ src/main/java/com/cotato/weather/domain/place/config/OpenWeatherMapApiConfig.java
@@ -0,0 +1,15 @@
+package com.cotato.weather.domain.place.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component @Getter @Setter
+@ConfigurationProperties(prefix = "open-weather-map")
+public class OpenWeatherMapApiConfig {
+    private String apiKey;
+    private String baseUrl;
+
+
+}
