d8ea8b37e518cda4eeaa946e95732fcf3c3533a2
u-genuine <u_genuine@naver.com>
2025-05-17 13:54:20 +0900

    feature: CORS 설정 추가

--- src/main/java/com/cotato/weather/global/config/auth/SecurityConfig.java
+++ src/main/java/com/cotato/weather/global/config/auth/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.cotato.weather.global.config.auth;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.cotato.weather.domain.user.enums.Role;
 
@@ -20,6 +26,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+			.cors(Customizer.withDefaults())
 			.csrf(csrf -> csrf.disable())
 			.headers(headers -> headers
 				.frameOptions(frameOptions -> frameOptions.disable())
@@ -35,5 +42,18 @@ public class SecurityConfig {
 			);
 
 		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트 주소
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		// configuration.setAllowCredentials(true); // 프론트에서 쿠키 등 자격정보 포함 시
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
