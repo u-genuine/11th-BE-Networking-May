0402f1512aa759c038120fc77a2ff4e882ea1593
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-05-17 11:42:40 +0900

    refactor : 임시 사용자 수정

--- src/main/java/com/cotato/weather/domain/place/service/PlaceService.java
+++ src/main/java/com/cotato/weather/domain/place/service/PlaceService.java
@@ -1,10 +1,11 @@
 package com.cotato.weather.domain.place.service;
 
 import com.cotato.weather.domain.place.dto.request.SavedPlaceCreateRequest;
+import com.cotato.weather.domain.place.dto.response.SavedPlaceListResponse;
 import com.cotato.weather.domain.place.entity.SavedPlace;
 import com.cotato.weather.domain.place.repository.SavedPlaceRepository;
-import com.cotato.weather.domain.user.entity.UserTemp;
-import org.springframework.security.core.userdetails.User;
+import com.cotato.weather.domain.user.entity.User;
+import com.cotato.weather.global.util.CurrentUserUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,26 +13,44 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class PlaceService {
-
+	private final CurrentUserUtil currentUserUtil;
 	private final SavedPlaceRepository savedPlaceRepository;
 
 	// 위치 저장
 	public Long savePlace(SavedPlaceCreateRequest savedPlaceCreateRequest) {
-		UserTemp user = new UserTemp(1L); //임시 사용자
+		User user = currentUserUtil.getCurrentUser();
 
-		SavedPlace savedPlace = user.createSavePlace(savedPlaceCreateRequest.getPlaceName(), savedPlaceCreateRequest.getX(), savedPlaceCreateRequest.getY());
+		SavedPlace savedPlace = SavedPlace.builder()
+				.user(user)
+				.placeName(savedPlaceCreateRequest.getPlaceName())
+				.x(savedPlaceCreateRequest.getX())
+				.y(savedPlaceCreateRequest.getY())
+				.isPinned(false) // 기본값 false로 설정
+				.build();
+
 
 		return savedPlaceRepository.save(savedPlace).getId();
 	}
 
 	// 위치 목록 조회
-	public List<SavedPlace> getSavedPlaces(Long userId) {
-		return savedPlaceRepository.findByUserTemp_Id(userId);
+	public List<SavedPlaceListResponse> getSavedPlaces() {
+		User user = currentUserUtil.getCurrentUser();
+		List<SavedPlace> savedPlaces =  savedPlaceRepository.findByUserId(user.getId());
+		return savedPlaces.stream()
+				.map(savedPlace -> SavedPlaceListResponse.builder()
+						.id(savedPlace.getId())
+						.placeName(savedPlace.getPlaceName())
+						.x(savedPlace.getX())
+						.y(savedPlace.getY())
+						.isPinned(savedPlace.isPinned())
+						.build())
+				.toList();
 	}
 
 	// 위치 핀 등록 및 해제
@@ -40,8 +59,10 @@ public class PlaceService {
 				.orElseThrow(() -> new IllegalArgumentException("SavedPlace not found with id: " + savedPlaceId));
 
 		// 핀 등록된 경우 해제, 해제된 경우 등록
-		UserTemp user = new UserTemp(1L);
-		user.updatePin(savedPlace);
+		User user = currentUserUtil.getCurrentUser();
+
+		savedPlace.updatePin(user);
+
 
 		savedPlaceRepository.save(savedPlace);
 	}
@@ -55,5 +76,10 @@ public class PlaceService {
 	public SavedPlace getSavedPlace(Long id) {
 		return savedPlaceRepository.findById(id)
 				.orElseThrow(() -> new IllegalArgumentException("SavedPlace not found with id: " + id));
+	}
+
+	//유저가 등록한 위치 조회
+	public List<SavedPlace> getSavedPlacesByUserId(Long userId) {
+		return savedPlaceRepository.findByUserId(userId);
 	}
 }
