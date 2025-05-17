868524d3380f147ea533393eee389a5c307ded12
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-05-17 13:29:36 +0900

    feat : converter 추가

---
+++ src/main/java/com/cotato/weather/global/converter/BooleanToYNConverter.java
@@ -0,0 +1,19 @@
+package com.cotato.weather.global.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class BooleanToYNConverter implements AttributeConverter<Boolean, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute) {
+        return attribute != null && attribute ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String dbData) {
+        return "Y".equals(dbData);
+    }
+
+}
