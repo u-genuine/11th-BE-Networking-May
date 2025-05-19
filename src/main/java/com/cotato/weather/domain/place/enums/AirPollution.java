a2c0f7552149cf0002e6a0b2b333c7b8cfc90ea0
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-05-19 18:09:00 +0900

    fix : AirPollution 범위 누락 수정

--- src/main/java/com/cotato/weather/domain/place/enums/AirPollution.java
+++ src/main/java/com/cotato/weather/domain/place/enums/AirPollution.java
@@ -6,18 +6,18 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum AirPollution {
-    GOOD("좋음", 0,30),
-    NORMAL("보통", 31,80),
-    BAD("나쁨", 81,150),
-    VERY_BAD("매우 나쁨", 151, 999);
+    GOOD("좋음", 0.0, 30.0),
+    NORMAL("보통", 30.0, 80.0),
+    BAD("나쁨", 80.0, 150.0),
+    VERY_BAD("매우 나쁨", 150.0, 999.0);
 
     private final String level;
-    private final int min;
-    private final int max;
+    private final double min;
+    private final double max;
 
     public static String getLevelByValue(double dust) {
         for (AirPollution airPollution : AirPollution.values()) {
-            if (dust >= airPollution.getMin() && dust <= airPollution.getMax()) {
+            if (dust >= airPollution.getMin() && dust < airPollution.getMax()) {
                 return airPollution.level;
             }
         }
