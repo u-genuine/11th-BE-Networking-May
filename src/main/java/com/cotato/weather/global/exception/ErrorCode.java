52bf4591c319dc497b9568da38ad52880be2e5ff
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-05-19 18:11:47 +0900

    feat : 에러코드 추가, 에러 핸들링 추가

--- src/main/java/com/cotato/weather/global/exception/ErrorCode.java
+++ src/main/java/com/cotato/weather/global/exception/ErrorCode.java
@@ -21,7 +21,15 @@ public enum ErrorCode {
 
 	// Place 관련 에러
 
-	// ..
+	// 외부 서버 에러
+	EXTERNAL_API_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "EXTERNAL-001", "외부 서버와의 통신에서 에러가 발생하였습니다."),
+	EXTERNAL_API_SERVER_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "EXTERNAL-002", "외부 API 서버와의 통신에서 인증 에러가 발생하였습니다."),
+	EXTERNAL_API_SERVER_EXCEED_RATE_LIMIT(HttpStatus.TOO_MANY_REQUESTS, "EXTERNAL-003", "외부 API 서버와의 통신에서 요청 한도를 초과하였습니다."),
+	EXTERNAL_API_SERVER_BAD_REQUEST(HttpStatus.BAD_REQUEST, "EXTERNAL-004", "외부 API 서버와의 통신에서 잘못된 요청이 발생하였습니다."),
+	EXETRNAL_API_SERVER_NOT_FOUND_LAT_LON(HttpStatus.NOT_FOUND, "EXTERNAL-005", "외부 API 서버와의 통신에서 요청한 리소스를 찾을 수 없습니다."),
+
+	// 파싱 에러
+	PARSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "PARSING-001", "데이터 파싱 중 에러가 발생하였습니다."),
 	;
 
 	private final HttpStatus httpStatus;
